6379e672020a615479d1e0e7cc08d5d85fc5a049
Tas Tasniem <tas.tasniem@beis.gov.uk>
2020-02-10 17:11:37 +0000

    Make rectangle clickable

--- control-assurance-sp/src/webparts/appHome/components/AppHome.tsx
+++ control-assurance-sp/src/webparts/appHome/components/AppHome.tsx
@@ -59,8 +59,9 @@ export default class AppHome extends BaseUserContextWebPartComponent<types.IWebP
               <img src={this.governanceApphomeImg}></img>
               <div>Governance</div>                
             </div>
-            <div style={{backgroundColor: 'rgb(122,116,117)', textAlign: 'center', padding: '10px' }} onClick={this.handleGovernanceClick} >
-              <a style={{color: 'white', fontSize: '25px', textDecoration: 'none', cursor: 'pointer' }} >Start</a>
+            <div style={{ backgroundColor: 'rgb(122,116,117)', textAlign: 'center', padding: '1px', cursor: 'pointer' }} 
+                    onClick={this.handleGovernanceClick}  >
+              <a style={{color: 'white', fontSize: '25px', textDecoration: 'none' }} >Start</a>
               
             </div>
           </div>
@@ -94,6 +95,11 @@ export default class AppHome extends BaseUserContextWebPartComponent<types.IWebP
     const pageUrl = this.props.spfxContext.pageContext.web.absoluteUrl + "/SitePages/GovernanceWelcome.aspx";
     window.location.href = pageUrl;
   }
+
+  private showHighlighted = (): void => {
+    
+  }
+  
 
   //#endregion event handlers
 
