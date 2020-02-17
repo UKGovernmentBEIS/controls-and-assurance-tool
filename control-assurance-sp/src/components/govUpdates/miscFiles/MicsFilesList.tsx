0dc78cb1abfbde6760d63a1194dba3f860f10f2d
Tas Tasniem <tas.tasniem@beis.gov.uk>
2020-02-17 16:43:06 +0000

    set _blank

--- control-assurance-sp/src/components/govUpdates/miscFiles/MicsFilesList.tsx
+++ control-assurance-sp/src/components/govUpdates/miscFiles/MicsFilesList.tsx
@@ -265,13 +265,14 @@ export default class MiscFilesList extends React.Component<IMiscFilesListProps, 
           const a = document.createElement('a');
           //document.body.appendChild(a);
           a.href = serverRelativeUrl;
-          //a.target = "_blank";
+          a.target = "_blank";
           a.download = fileName;
           
           document.body.appendChild(a);
+          console.log(a);
           a.click();
           document.body.removeChild(a);
-
+          
           /*
           setTimeout(() => {
             //window.URL.revokeObjectURL(serverRelativeUrl);
