9fd969ef8e85aa1204dc7e8620b495b18118e016
Tas Tasniem <tas.tasniem@beis.gov.uk>
2020-03-13 13:36:50 +0000

    Use Server Relative Path

--- control-assurance-sp/src/types/AppGlobals.ts
+++ control-assurance-sp/src/types/AppGlobals.ts
@@ -8,16 +8,16 @@ import { WebPartContext } from "@microsoft/sp-webpart-base";
 //export const UploadFolder_Evidence:string = "/sites/site3/Shared%20Documents/Evidence";
 
 export function getUploadFolder_MiscFiles(context: WebPartContext) : string {
-    const webTitle = context.pageContext.web.absoluteUrl;
-    console.log("props.spfxContext.pageContext.web.title ", `'${webTitle}'`);
-    const path:string = `/sites/${webTitle}/Shared%20Documents/MiscFiles`;
+    const serverRelativeUrl = context.pageContext.web.serverRelativeUrl;
+    console.log("props.spfxContext.pageContext.web.serverRelativeUrl ", `'${serverRelativeUrl}'`);
+    const path:string = `${serverRelativeUrl}/Shared%20Documents/MiscFiles`;
     return path;
 }
 
 export function getUploadFolder_Evidence(context: WebPartContext) : string {
-    const webTitle = context.pageContext.web.serverRelativeUrl;
-    console.log("props.spfxContext.pageContext.web.title ", `'${webTitle}'`);
-    const path:string = `/sites/${webTitle}/Shared%20Documents/Evidence`;
+    const serverRelativeUrl = context.pageContext.web.serverRelativeUrl;
+    console.log("props.spfxContext.pageContext.web.serverRelativeUrl ", `'${serverRelativeUrl}'`);
+    const path:string = `${serverRelativeUrl}/Shared%20Documents/Evidence`;
     return path;
 }
 
