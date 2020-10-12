d5a8494dd58fb9f6bcc7d48ebffd3fdf71af91c9
Tas Tasniem <tas.tasniem@beis.gov.uk>
2020-10-12 12:14:23 +0100

    Download Excel issue

--- control-assurance-sp/src/webparts/orgReport/OrgReportWebPart.ts
+++ control-assurance-sp/src/webparts/orgReport/OrgReportWebPart.ts
@@ -2,11 +2,25 @@ import * as React from 'react';
 import OrgReport from './components/OrgReport';
 import { IWebPartComponentProps } from '../../types';
 import DataAPIWebPart from '../DataAPIWebPart';
+import { sp } from "@pnp/sp";
 
 
 
 export default class OrgReportWebPart extends DataAPIWebPart {
 	protected WebPartDescription = 'Web part for Org Reports';
+
+	public onInit(): Promise<void> {
+
+		return super.onInit().then(_ => {
+	  
+		  // other init code may be present
+		  console.log('on init');
+	  
+		  sp.setup({
+			spfxContext: this.context
+		  });
+		});
+	  }
 
 	protected renderWebPart(): React.ReactElement<IWebPartComponentProps> {
 		return React.createElement(
