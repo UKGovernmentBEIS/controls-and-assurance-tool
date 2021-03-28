597e477ab42239007ffa2428e046c5a201555054
Tas Tasniem <tas.tasniem@beisdigitaldev.onmicrosoft.com>
2021-03-28 19:35:00 +0100

    CL Leaving pass checked by now the last row

--- control-assurance-sp/src/components/CL/NewCaseTab.tsx
+++ control-assurance-sp/src/components/CL/NewCaseTab.tsx
@@ -5239,6 +5239,23 @@ export default class NewCaseTab extends React.Component<INewCaseTabProps, INewCa
                             </tr>
 
                             <tr>
+                                <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', backgroundColor: 'rgb(229,229,229)', borderBottom: '1px solid rgb(166,166,166)' }}>
+                                    Pass checked by
+                                </td>
+                                <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', borderBottom: '1px solid rgb(166,166,166)' }}>
+                                    {caseInfo.LePassCheckedBy}
+                                </td>
+                                <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', backgroundColor: 'rgb(229,229,229)', borderBottom: '1px solid rgb(166,166,166)' }}>
+                                    Pass checked on
+                                </td>
+                                <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', borderRight: '1px solid rgb(166,166,166)', borderBottom: '1px solid rgb(166,166,166)' }}>
+                                    {caseInfo.LePassCheckedOn}
+                                </td>
+
+                            </tr>
+
+                            {/*
+                            <tr>
                                 <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', backgroundColor: 'rgb(229,229,229)' }}>
                                     Pass checked by
                                 </td>
@@ -5253,7 +5270,7 @@ export default class NewCaseTab extends React.Component<INewCaseTabProps, INewCa
                                 </td>
 
                             </tr>
-                            {/*
+                            
                             <tr>
                                 <td style={{ borderTop: '1px solid rgb(166,166,166)', borderLeft: '1px solid rgb(166,166,166)', backgroundColor: 'rgb(229,229,229)', borderBottom: '1px solid rgb(166,166,166)' }}>
                                     Contract checked by
