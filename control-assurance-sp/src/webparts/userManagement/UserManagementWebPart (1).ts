c402b03c2f999e22bcf4e3ad59a628d9a260764c
TasTasniem <97549043+TasTasniem@users.noreply.github.com>
2022-06-30 16:38:10 +0100

    Changed logger

---
+++ control-assurance-sp/src/webparts/userManagement/UserManagementWebPart (1).ts
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import UserManagement from './components/UserManagement';
+import { IWebPartComponentProps } from '../../types';
+import DataAPIWebPart from '../DataAPIWebPart';
+import { sp } from "@pnp/sp";
+
+
+
+export default class UserManagementWebPart extends DataAPIWebPart {
+	protected WebPartDescription = 'Web part for user management';
+
+	public onInit(): Promise<void> {
+
+		return super.onInit().then(_ => {
+	  
+		  // other init code may be present
+		  console.log('on init');
+	  
+		  sp.setup({
+			spfxContext: this.context
+		  });
+		});
+	  }
+
+	protected renderWebPart(): React.ReactElement<IWebPartComponentProps> {
+		return React.createElement(
+			UserManagement,
+			{
+				spfxContext: this.context,
+				api: this.api
+			}
+		);
+	}
+}
+
+
