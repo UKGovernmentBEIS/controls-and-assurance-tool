5fd058c955cfacfb47b18d609656de47ef8db827
TasTasniem <97549043+TasTasniem@users.noreply.github.com>
2023-05-30 11:45:08 +0100

    New permission canmanageusers can now added permissions less powerful than itself

--- ControlAssuranceAPI/Repositories/UserPermissionRepository.cs
+++ ControlAssuranceAPI/Repositories/UserPermissionRepository.cs
@@ -26,43 +26,43 @@ namespace ControlAssuranceAPI.Repositories
 
         public bool CheckEditDelPermission(int key)
         {
-            //db.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
-            int userId = ApiUser.ID;
-
-            bool superUser = false;
-            bool moduleSuperUser = false;
-
-            var userPermissions = db.UserPermissions.Where(up => up.UserId == userId).ToList();
-            foreach (var permissioin in userPermissions)
+            var requestPermissionTypeId = db.UserPermissions.FirstOrDefault(up => up.ID == key)?.PermissionTypeId;
+            if (requestPermissionTypeId != null)
             {
-                if (permissioin.PermissionTypeId == 1)
-                {
-                    superUser = true;
-                }
-                else if (permissioin.PermissionTypeId == 5 || permissioin.PermissionTypeId == 6 || permissioin.PermissionTypeId == 7 || permissioin.PermissionTypeId == 8 || permissioin.PermissionTypeId == 11 || permissioin.PermissionTypeId == 16)
-                {
-                    moduleSuperUser = true;
-                }
-            }
+                int userId = ApiUser.ID;
 
-            if (superUser == true)
-                return true;
+                bool superUser = false;
+                bool moduleSuperUser = false;
+                bool canManageUsers = false;
 
-            else if (moduleSuperUser == true)
-            {
-                var userPermission = db.UserPermissions.FirstOrDefault(up => up.ID == key);
-                //module super user can't edit/del any UserPermission of type Super User (main super user or module super user)
-                if (userPermission.PermissionTypeId == 1 || userPermission.PermissionTypeId == 5 || userPermission.PermissionTypeId == 6 || userPermission.PermissionTypeId == 7 || userPermission.PermissionTypeId == 8 || userPermission.PermissionTypeId == 11 || userPermission.PermissionTypeId == 16)
+                var userPermissions = db.UserPermissions.Where(up => up.UserId == userId).ToList();
+                foreach (var permissioin in userPermissions)
                 {
-                    return false;
+                    if (permissioin.PermissionTypeId == 1)
+                        superUser = true;
+                    else if (permissioin.PermissionTypeId == 5 || permissioin.PermissionTypeId == 6 || permissioin.PermissionTypeId == 7 || permissioin.PermissionTypeId == 8 || permissioin.PermissionTypeId == 11)
+                        moduleSuperUser = true;
+                    else if (permissioin.PermissionTypeId == 16)
+                        canManageUsers = true;
                 }
-                else
-                {
+
+                if (superUser)
                     return true;
+
+                else if (moduleSuperUser)
+                {
+                    //module super user can't edit/del any UserPermission of type Super User (main super user or module super user)
+                    if (requestPermissionTypeId == 1 || requestPermissionTypeId == 5 || requestPermissionTypeId == 6 || requestPermissionTypeId == 7 || requestPermissionTypeId == 8 || requestPermissionTypeId == 11 || requestPermissionTypeId == 13 || requestPermissionTypeId == 16)
+                        return false;
+                    else                    
+                        return true;
                 }
-
+                else if (canManageUsers)
+                {
+                    // canManageUsers role can't edit/delete any UserPermission of the following types
+                    return !new[] { 1, 2, 5, 6, 7, 8, 11, 13, 16 }.Any(id => id == requestPermissionTypeId);
+                }
             }
-
             return false;
 
         }
